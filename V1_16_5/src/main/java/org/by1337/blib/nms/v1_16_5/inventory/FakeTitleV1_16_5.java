05b733c68eabd53adb4c336687b97d81e5b1658f
By1337 <by133700@gmail.com>
2024-03-21 02:57:47 +0300

    feat(FakeTitle): add support Component

--- V1_16_5/src/main/java/org/by1337/blib/nms/v1_16_5/inventory/FakeTitleV1_16_5.java
+++ V1_16_5/src/main/java/org/by1337/blib/nms/v1_16_5/inventory/FakeTitleV1_16_5.java
@@ -1,7 +1,9 @@
 package org.by1337.blib.nms.v1_16_5.inventory;
 
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.minecraft.server.v1_16_R3.ChatComponentText;
 import net.minecraft.server.v1_16_R3.EntityPlayer;
+import net.minecraft.server.v1_16_R3.IChatBaseComponent;
 import net.minecraft.server.v1_16_R3.PacketPlayOutOpenWindow;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_16_R3.util.CraftChatMessage;
@@ -14,15 +16,28 @@ import java.util.List;
 
 public class FakeTitleV1_16_5 implements FakeTitle {
     @Override
+    @Deprecated(since = "1.0.7")
     public void send(Inventory inventory, String newTitle) {
+        send(inventory, CraftChatMessage.fromStringOrNull(newTitle));
+    }
+
+    public void send(Inventory inventory, IChatBaseComponent newTitle) {
         List<HumanEntity> list = new ArrayList<>(inventory.getViewers());
         for (HumanEntity humanEntity : list) {
             if (humanEntity instanceof CraftPlayer craftPlayer) {
                 EntityPlayer entityPlayer = craftPlayer.getHandle();
-                PacketPlayOutOpenWindow packet = new PacketPlayOutOpenWindow(entityPlayer.activeContainer.windowId, entityPlayer.activeContainer.getType(), CraftChatMessage.fromStringOrNull(newTitle));
+                PacketPlayOutOpenWindow packet = new PacketPlayOutOpenWindow(entityPlayer.activeContainer.windowId,
+                        entityPlayer.activeContainer.getType(),
+                        newTitle);
                 entityPlayer.playerConnection.sendPacket(packet);
                 entityPlayer.updateInventory(entityPlayer.activeContainer);
             }
         }
+    }
+
+    @Override
+    public void send(Inventory inventory, net.kyori.adventure.text.Component newTitle) {
+        IChatBaseComponent iChatBaseComponent = IChatBaseComponent.ChatSerializer.a(GsonComponentSerializer.gson().serialize(newTitle));
+        send(inventory, iChatBaseComponent);
     }
 }
